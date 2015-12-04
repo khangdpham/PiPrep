4844c97ac1072cef38387366f981c50cc67d32e5
Khang Pham <khangpham@ymail.com>
2015-12-04 12:17:50 -0800

    Update PiPrep.sh

--- PiPrep.sh
+++ PiPrep.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-sudo apt-get purge libreoffice wolfram-engine sonic-pi scratch
-sudo apt-get autoremove
+apt-get purge libreoffice wolfram-engine sonic-pi scratch
+apt-get autoremove
+apt-get update
+apt-get upgrade
